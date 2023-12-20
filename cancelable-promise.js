dc3d03dbf9d4fe2fdecc04fc76ca89c58290655a
Oleksandr Riabokin <alex1rap.wen.ru@gmail.com>
2023-12-20 22:21:23 +0200

    Update cancelable-promise.js
    Fixed for tests passing

--- cancelable-promise.js
+++ cancelable-promise.js
@@ -1,76 +1,124 @@
 class CancelablePromise {
   constructor(executor) {
+    if (typeof executor !== "function") {
+      throw new Error("Executor must be a function");
+    }
+
     this.isCanceled = false;
-    this.promise = new Promise((resolve, reject) => {
-      this.executor = (onFulfilled, onRejected) => {
-        let logIsCancelled = false;
-        if (!this.isCanceled) {
+    this.cancel = this.cancel.bind(this);
+
+    this.executor = (onFulfilled, onRejected) => {
+      if (!this.isCanceled) {
+        try {
           executor(
             (value) => {
               if (!this.isCanceled) {
-                resolve(value);
+                onFulfilled(value);
               }
             },
             (reason) => {
               if (!this.isCanceled) {
-                reject(reason);
+                onRejected(reason);
               }
-            }
+            },
           );
+        } catch (error) {
+          onRejected(error);
         }
-      };
-    });
+      } else {
+        onRejected({ isCanceled: true });
+      }
+    };
 
-    this.cancel = this.cancel.bind(this);
-    this.execute = this.execute.bind(this);
+    this.promise = new Promise((resolve, reject) => {
+      this.executor(
+        (value) => {
+          if (!this.isCanceled) {
+            resolve(value);
+          } else {
+            reject({ isCanceled: true });
+          }
+        },
+        (reason) => {
+          if (!this.isCanceled) {
+            reject(reason);
+          } else {
+            reject({ isCanceled: true });
+          }
+        },
+      );
+    });
   }
 
   cancel() {
     this.isCanceled = true;
   }
 
-  execute(onFulfilled, onRejected) {
-    this.executor(onFulfilled, onRejected);
+  then(onFulfilled, onRejected) {
+    return new CancelablePromise((resolve, reject) => {
+      this.promise.then(
+        (value) => {
+          if (this.isCanceled) {
+            reject({ isCanceled: true });
+          } else {
+            try {
+              resolve(onFulfilled ? onFulfilled(value) : value);
+            } catch (error) {
+              reject(error);
+            }
+          }
+        },
+        (reason) => {
+          if (onRejected) {
+            try {
+              resolve(onRejected(reason));
+            } catch (error) {
+              reject(error);
+            }
+          } else {
+            reject(reason);
+          }
+        },
+      );
+    });
   }
 
-  then(onFulfilled, onRejected) {
-    return this.promise.then(onFulfilled, onRejected);
+  execute(onFulfilled, onRejected) {
+    this.executor(onFulfilled || (() => {}), onRejected || (() => {}));
   }
 
   catch(onRejected) {
-    return this.promise.catch(onRejected);
+    return new CancelablePromise((resolve, reject) => {
+      this.promise.catch((reason) => {
+        if (this.isCanceled) {
+          reject({ isCanceled: true });
+        } else {
+          try {
+            resolve(onRejected ? onRejected(reason) : reason);
+          } catch (error) {
+            reject(error);
+          }
+        }
+      });
+    });
   }
 
   finally(onFinally) {
-    return this.promise.finally(onFinally);
+    return new CancelablePromise((resolve, reject) => {
+      this.promise.finally(() => {
+        if (this.isCanceled) {
+          reject({ isCanceled: true });
+        } else {
+          try {
+            onFinally();
+            resolve();
+          } catch (error) {
+            reject(error);
+          }
+        }
+      });
+    });
   }
 }
 
-// Приклад використання:
-let executeAfter =  Math.random() * 4000 + 1000;
-let cancelAfter = Math.random() * 4000 + 1300;
-
-console.log({executeAfter, cancelAfter});
-
-const cancelablePromise = new CancelablePromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Проміс виконано');
-  }, executeAfter);
-});
-
-cancelablePromise.execute(
-  (result) => console.log(result),
-  (error) => console.error(error)
-); // Запуск виконання
-
-setTimeout(() => {
-    // Для скасування ланцюга промісів перед виконанням
-    cancelablePromise.cancel();
-    console.log('Спроба скасувати проміс');
-  }, cancelAfter);
-
-// Використання then, catch та finally
-cancelablePromise
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error))
-  .finally(() => console.log('Блок finally виконано'));
+module.exports = CancelablePromise;
